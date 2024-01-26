433f0afacf237c3834db0f2ab74ff37277de35be
Sri Krishna Bejawada <21a31a05a3@pragati.ac.in>
2024-01-26 23:14:54 +0530

    PREV & NEXT Btn  added

--- src/Components/News.js
+++ src/Components/News.js
@@ -3,24 +3,47 @@ import NewsItem from './NewsItem';
 
 const News = () => {
   const [articles, setArticles] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0)
+const handleOnNext = async ()=>{
+  setPage(page+1);
+}
+const handleOnPrev = async ()=>{
+  setPage(page-1);
+}
+
+// const maxPages = async ()=>{
+   let pageSize = 12
+//   if(Math.ceil(totalResults/pageSize)+1<page){
+//     return true;
+//   }
+//   return false;
+// }
 
   useEffect(() => {
     const fetchData = async () => {
-        let fetched = await fetch("https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=a90ea2fa560b44bdade55343ba306110");
+        let fetched = await fetch(`https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=a90ea2fa560b44bdade55343ba306110&page=${page}&pageSize=12`);
         let data = await fetched.json();
         setArticles(data.articles);
-        console.log(data.articles);
+        console.log(page);
+        setTotalResults(data.totalResults);
+        console.log("Total results"+ totalResults)
     };
-
     fetchData();
-  }, []);
+  }, [page]);
 
   return (
     <div className="container my-3">
+    {/* ________________________________________________________ */}
+    <div className='buttons d-flex justify-content-around'>
+        <button type="button" disabled={page<2} onClick={handleOnPrev} className="btn btn-success"> &larr; Prev.</button>
+        <button type="button" onClick={handleOnNext} disabled={totalResults<(page)*pageSize} className="btn btn-success">Next &rarr;</button>
+      </div>  
+    {/* ______________________________________________________ */}
       <h2>News Monkey - Top Headlines</h2>
       <div className='row'>
         {articles.map((ele, index) => (
-          <div className='col-md-3' key={index}>
+          <div className='col-md-4' key={index}>
             <NewsItem
               title={ele.title}
               description={ele.description}
@@ -30,6 +53,10 @@ const News = () => {
             />
           </div>
         ))}
+      </div>
+      <div className='buttons d-flex justify-content-around'>
+        <button type="button" disabled={page<2} onClick={handleOnPrev} className="btn btn-success"> &larr; Prev.</button>
+        <button type="button" onClick={handleOnNext} disabled={totalResults<(page)*pageSize} className="btn btn-success">Next &rarr;</button>
       </div>
     </div>
   );
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -7,7 +7,7 @@ export class NewsItem extends Component {
     return (
       <div>
           <div className="row mx-3">
-                <div className="col-md-3 my-3 ">
+                <div className="col-md-3 my-4">
                     <div className="card" style={{width: "18rem"}}>
                         <img src={imgUrl?imgUrl:"https://images.theconversation.com/files/377569/original/file-20210107-17-q20ja9.jpg?ixlib=rb-1.1.0&rect=278%2C340%2C4644%2C3098&q=45&auto=format&w=926&fit=clip"} className="card-img-top" alt="..."/>
                         <div className="card-body">
